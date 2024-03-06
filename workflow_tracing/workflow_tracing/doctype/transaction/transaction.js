9182ae805854809b3fd9425412fd80bda69064a9
ZGalsaqqaf <zrz10302021glg@gmail.com>
2024-03-06 10:33:45 +0300

    feat(Core): Update Transaction doctype adding fields and changes
    - Enable selection of only one checkbox between Outgoing and Incoming
    
    - Set Start Date field to today's date by default
    
    - Add Urgency select field with options: Low, Medium, High, Urgent. And change field name to Priority
    
    - Add Company field to Transaction doctype

--- workflow_tracing/workflow_tracing/doctype/transaction/transaction.js
+++ workflow_tracing/workflow_tracing/doctype/transaction/transaction.js
@@ -6,3 +6,27 @@
 
 // 	},
 // });
+
+// restrect choosing of Incoming and Outgoing check box
+frappe.ui.form.on('Transaction', {
+    refresh: function(frm) {
+        frm.toggle_reqd('incoming', !frm.doc.outgoing);
+    },
+    outgoing: function(frm) {
+        frm.doc.incoming = !frm.doc.outgoing;
+        frm.refresh_field('incoming');
+    },
+    incoming: function(frm) {
+        frm.doc.outgoing = !frm.doc.incoming;
+        frm.refresh_field('outgoing');
+    }
+});
+
+// set the "Start Date" field to the current date by default 
+frappe.ui.form.on('Transaction', {
+    onload: function(frm) {
+        if (!frm.doc.start_date) {
+            frm.set_value('start_date', frappe.datetime.get_today());
+        }
+    }
+});
